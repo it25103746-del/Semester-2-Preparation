9be616c9389923ce2f8c7fe0a2d2add67da02cae
it25103746-del <it25103746@my.sliit.lk>
2026-02-08 23:53:49 +0530

    Lab01- Question04

---
+++ IT25103746Q4.java
@@ -0,0 +1,26 @@
+public class IT25103746Q4{
+    public static void main(String[] args) {
+        System.out.println("Using while loop:");
+        int row = 0;
+        while (row < 5) {
+            int col = 0;
+            while (col < 5) {
+                System.out.print("* ");
+                col++;
+            }
+            System.out.println();
+            row++;
+        }
+
+        System.out.println("\nUsing for loop: ");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

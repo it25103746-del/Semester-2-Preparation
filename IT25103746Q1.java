ae77c23314639e8ab8f80e35fe02e8a393688d9b
it25103746-del <it25103746@my.sliit.lk>
2026-02-08 23:14:46 +0530

    Lab01- Question01

---
+++ IT25103746Q1.java
@@ -0,0 +1,16 @@
+public class IT25103774Q1{
+    public static void main(String[] args) {
+        // a) & b) Declare and initialize variables
+        int miles = 26;
+        int yards = 385;
+        double kilometers;
+
+        // c) & d) Calculate and save result
+        kilometers = (miles + (yards / 1768.0)) * 1.609;
+        System.out.println("Total Kilometers: " + kilometers);
+    }
+}
+
+
+
+

74892fdf05e6edb37e5c7520ba1feaddcdba8aa1
it25103746-del <it25103746@my.sliit.lk>
2026-02-08 23:53:14 +0530

    Lab01- Question02

---
+++ IT25103746Q2.java
@@ -0,0 +1,18 @@
+public class IT25103746Q2{
+    public static void main(String[] args) {
+        int[] A = {10, 20, 30, 40, 50};
+        int[] B = {34, 67, 12, 89, 12};
+        int[] C = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            C[i] = A[i] + B[i];
+        }
+
+        System.out.print("Array C: [");
+        for (int i = 0; i < C.length; i++) {
+            System.out.print(C[i] + (i == C.length - 1 ? "" : ", "));
+        }
+        System.out.println("]");
+    }
+}
+
